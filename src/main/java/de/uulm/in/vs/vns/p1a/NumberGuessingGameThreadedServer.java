6868f641bf5034c5907d2142f4c4a95696629416
Benedict <mampfred33@gmail.com>
2023-10-19 10:19:17 +0200

    Implemented player maximum with FixedThreadPool

--- src/main/java/de/uulm/in/vs/vns/p1a/NumberGuessingGameThreadedServer.java
+++ src/main/java/de/uulm/in/vs/vns/p1a/NumberGuessingGameThreadedServer.java
@@ -3,22 +3,25 @@ package de.uulm.in.vs.vns.p1a;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NumberGuessingGameThreadedServer {
     public static void main(String[] args) throws IOException {
 
         ServerSocket serverSocket = new ServerSocket(5555);
         final int MAX_PLAYERS = 4;
-        int currentPlayers = 0;
+        ExecutorService threadPool = Executors.newFixedThreadPool(MAX_PLAYERS);
         while (true) {
-            if(currentPlayers < MAX_PLAYERS) {
-                System.out.println("Waiting for connection");
-                Socket connectionSocket = serverSocket.accept();
-                System.out.println("Connection accepted");
-                NumberGuessingGameRequestHandler handler = new NumberGuessingGameRequestHandler(connectionSocket);
-                new Thread(handler).start();
-                currentPlayers++;
-            }
+            /*System.out.println("Waiting for connection");
+            Socket connectionSocket = serverSocket.accept();
+            System.out.println("Connection accepted");
+            NumberGuessingGameRequestHandler handler = new NumberGuessingGameRequestHandler(connectionSocket);
+            new Thread(handler).start();*/
+            System.out.println("Waiting for connection");
+            Socket connectionSocket = serverSocket.accept();
+            System.out.println("Connection accepted");
+            threadPool.execute(new NumberGuessingGameRequestHandler(connectionSocket));
         }
     }
 }
