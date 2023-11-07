48ba70e35036fdbe0f72a399c811f1cd4d83ecb4
mumian <mumian.haas@uni-ulm.de>
2023-11-07 10:17:59 +0100

    test

---
+++ src/main/java/de/uulm/in/vs/vns/p2a/URLFetcher.java
@@ -0,0 +1,14 @@
+package de.uulm.in.vs.vns.p2a;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class URLFetcher {
+    public static void main(String[] args) {
+
+    }
+}
