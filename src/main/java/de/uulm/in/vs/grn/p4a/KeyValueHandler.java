939d7673815b7e203d1f87d3c636bc41ffc6cd94
Mumian <mumian.haas@uni-ulm.de>
2023-12-04 22:09:09 +0100

    Added ThreadPool Executor Service with shareMap and no Concurrency Guards

---
+++ src/main/java/de/uulm/in/vs/grn/p4a/KeyValueHandler.java
@@ -0,0 +1,91 @@
+package de.uulm.in.vs.grn.p4a;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Map;
+
+public class KeyValueHandler implements Runnable {
+
+
+    private Map<String,String> map;
+    Socket connectionSocket;
+
+    BufferedReader reader;
+    BufferedWriter writer;
+    public KeyValueHandler(Socket connectionSocket,Map<String,String> sharedMap) throws IOException{
+        this.connectionSocket = connectionSocket;
+        reader = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(connectionSocket.getOutputStream()));
+        this.map = sharedMap;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String input;
+            try {
+                input = reader.readLine();
+            } catch (
+                    IOException e) {
+                throw new RuntimeException(e);
+            }
+            outer:
+            while (input != null) {
+
+                try {
+                    String[] inputs_a = input.split(" ");
+
+                    String Command = inputs_a[0];
+                    switch (Command) {
+                        case "GET":
+                            if (inputs_a.length != 2) {
+                                writer.write("ERR: Unknown Command!\n");
+                                break;
+                            }
+                            if (map.containsKey(inputs_a[1])) {
+                                writer.write("RES: " + map.get(inputs_a[1]));
+                                writer.newLine();
+                                break;
+                            } else {
+                                writer.write("ERR: Unknown Key!\n");
+                                break;
+                            }
+                        case "PUT":
+                            if (inputs_a.length != 3) {
+                                writer.write("ERR: Unknown Command!\n");
+                            } else {
+                                writer.write("RES: OK");
+                                map.put(inputs_a[1], inputs_a[2]);
+
+                            }
+                            break;
+                        case "EXIT":
+                            writer.write("RES: BYE!\n");
+                            break outer;
+                        default:
+                            writer.write("ERR: Unknown Command!\n");
+                    }
+
+
+                } finally {
+                    writer.flush();
+
+                }
+                try {
+                    input = reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException();
+                }
+
+            }
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connectionSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/de/uulm/in/vs/grn/p4a/keyValueServer.java
+++ src/main/java/de/uulm/in/vs/grn/p4a/keyValueServer.java
@@ -5,6 +5,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 //import java.net.
 public class keyValueServer {
@@ -15,73 +17,18 @@ public class keyValueServer {
         ServerSocket serverSocket = new ServerSocket(3211);
 
         Map<String, String> map = new HashMap<>();
-
+        final int MAX_CONNECTIONS = 4;
+        ExecutorService threadPool = Executors.newFixedThreadPool(MAX_CONNECTIONS);
 
         while(true){
 
             System.out.println("Waiting for connection");
-            Socket connectionsSocket = serverSocket.accept();
+            Socket connectionSocket = serverSocket.accept();
             System.out.println("Connection accepted");
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connectionsSocket.getInputStream()));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connectionsSocket.getOutputStream()));
-            String input;
-            try {
-                input = reader.readLine();
-            } catch (IOException e){
-                throw new RuntimeException(e);
-            }
-            outer:
-            while(input != null){
+            threadPool.execute(new KeyValueHandler(connectionSocket, map));
+            //BufferedReader reader = new BufferedReader(new InputStreamReader(connectionsSocket.getInputStream()));
+            //BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connectionsSocket.getOutputStream()));
 
-                try {
-                    String[] inputs_a = input.split(" ");
-
-                    String Command = inputs_a[0];
-                    switch (Command) {
-                        case "GET":
-                            if (inputs_a.length != 2) {
-                                writer.write("ERR: Unknown Command!\n");
-                                break;
-                            }
-                            if (map.containsKey(inputs_a[1])) {
-                                writer.write("RES: " + map.get(inputs_a[1]));
-                                writer.newLine();
-                                break;
-                            }
-                            else {
-                                writer.write("ERR: Unknown Key!\n");
-                                break;
-                            }
-                        case "PUT":
-                            if (inputs_a.length != 3){
-                                writer.write("ERR: Unknown Command!\n");
-                            }
-                            else {
-                                map.put(inputs_a[1], inputs_a[2]);
-
-                            }
-                            break;
-                        case "EXIT":
-                            writer.write("RES: BYE!\n");
-                            break outer;
-                        default:
-                            writer.write("ERR: Unknown Command!\n");
-                    }
-
-
-                } finally {
-                    writer.flush();
-
-                }
-                try {
-                    input = reader.readLine();
-                } catch (IOException e){
-                    throw new RuntimeException();
-                }
-
-            }
-
-            connectionsSocket.close();
 
 
         }
